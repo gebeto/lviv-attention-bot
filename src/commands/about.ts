39b4a4b71531dd456e109c04ff126d84e3af0dbe
gebeto <slavik.nychkalo@gmail.com>
2025-06-10 00:27:46 +0300

    add webhook details

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -8,7 +8,15 @@ const debug = createDebug('bot:about_command');
 const about = () => async (ctx: Context) => {
   const message = `*${name} ${version}*\n${author}`;
   debug(`Triggered "about" command with message \n${message}`);
-  await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
+  const details = JSON.stringify(
+    await ctx.telegram.getWebhookInfo(),
+    undefined,
+    2,
+  );
+  const detailsMD = '```' + details + '```';
+  await ctx.replyWithMarkdownV2(message + '\n\n' + detailsMD, {
+    parse_mode: 'Markdown',
+  });
 };
 
 export { about };
